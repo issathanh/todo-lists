d22b177cc7e7ee979bbbca15a0d06b4ebc673d37
Thanh <thanh.thuonq09@gmail.com>
2025-07-06 20:06:56 -0500

    add display function to display todo task

--- src/dom.js
+++ src/dom.js
@@ -13,9 +13,50 @@ document.addEventListener('DOMContentLoaded', function(){
         //create new todo
         const todo = TodoManager.createItem(title,description,priority)
         //test 
-        console.log('todo created', todo.getAllInfo())
-        console.log('All todos: ', TodoManager.getAllTodos())
-
+        displayTodoCard(todo)
+        
         form.reset()
     })
 })
+
+function createTodoCard(todo){
+    //1. Create the card element
+    let card = document.createElement('div')
+    card.className = 'todo-card'
+    //2. Add todo information (title, desccription, priority)
+    const todoInfo = todo.getAllInfo()
+    let title = document.createElement('h2')
+    title.textContent = todoInfo.title
+    card.appendChild(title)
+
+    let description = document.createElement('p')
+    description.textContent = todoInfo.description
+    card.appendChild(description)
+
+    let status = document.createElement('p')
+    status.textContent = todoInfo.complete? 'Completed': 'Pending'
+    status.style.color = todoInfo.complete? 'green':'orange'
+    card.appendChild(status)
+    //3. Addd the three button (delete, cpmplete, edit)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Delete'
+
+    let completeBtn = document.createElement('button')
+    completeBtn.textContent = 'Complete'
+
+    let editBtn = document.createElement('button')
+    editBtn.textContent = 'Edit'
+    
+    card.appendChild(deleteBtn)
+    card.appendChild(completeBtn)
+    card.appendChild(editBtn)
+    //4. Return the completed card element 
+    return card
+}
+
+function displayTodoCard(todo){
+    const card = createTodoCard(todo)
+    const todosDisplay = document.getElementById('todos-display')
+    todosDisplay.appendChild(card)
+}
